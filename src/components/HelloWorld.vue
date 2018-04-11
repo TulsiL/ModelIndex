bd723392ecb982c0554993abc723e6d0f501e263
root <root@localhost.localdomain>
2018-04-11 15:06:57 +0530

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main">
+   
+    <!-- <div style="color:red">{{counter}}</div> -->
+    <br/>
+    <div v-for="n in 4" :key="n.id">
+    <label style="color:pink">{{counter++}}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {     
+        counter: 0      
+    }
+  },
+  watch:{
+    counter() {
+      console.log(this.counter);      
+      this.counter=0;
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
