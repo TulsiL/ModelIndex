a4dd2f9a1c07217805328b77f206be00dc926037
root <root@localhost.localdomain>
2018-05-01 14:08:01 +0530

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import EventsDemo from '@/components/EventsDemo'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'EventsDemo',
-      component: EventsDemo
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
